e7636d980ecabf51ed98d3b1485198e47b478a93
abdul-muyeez <abdulmuyeez1406@gmail.com>
2024-07-17 19:28:40 +0600

    1st commit

---
+++ src/test/java/webauto/bd/utilities/GetScreenShot.java
@@ -0,0 +1,27 @@
+package webauto.bd.utilities;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class GetScreenShot {
+	
+	public static String capture(WebDriver driver, String screenShot) throws IOException {
+		
+		TakesScreenshot ts =(TakesScreenshot) driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		String dest = System.getProperty("user.dir")+"\\screenshot\\"+screenShot+".png";
+		File destination = new File(dest);
+		FileUtils.copyFile(source, destination);
+		return dest;
+		
+		
+		
+		
+	}
+
+}
